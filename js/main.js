96152d569e086861f76b50b450e8c8d7e038fb58
ArmandoArV <armando.arredondo.valle@gmail.com>
2022-09-20 16:03:34 -0500

    Added the register.html

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
-// add inside the id login add the login function with the username and password
-// add inside the id register add the register function with the username and password
+let users = [
+    { name: 'John', password: "abcd" },
+    { name: 'TonyNogueron', password: "1234" },
+];
+
+
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    for (let i = 0; i < users.length; i++) {
+        if (username === users[i].name && password === users[i].password) {
+            window.location.href = "mainUI.html";
+            return;
+        }
+    }
+    alert("Username or password is incorrect");
+}
+
+function register() {
+    window.location.href = "register.html";
+}
