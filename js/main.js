23eb7e44434d7a9c178ecf84ccdaff55d490eb16
ArmandoArV <armando.arredondo.valle@gmail.com>
2022-09-21 08:13:11 -0500

    @TonyNogueron modifications inside the index, style, mainUI

--- js/main.js
+++ js/main.js
@@ -19,3 +19,25 @@ function login() {
 function register() {
     window.location.href = "register.html";
 }
+
+function addRegister() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const passwordConfirmation = document.getElementById("confirmPassword").value;
+    if (password !== passwordConfirmation) {
+        alert("Passwords do not match");
+        return;
+    }
+    for (let i = 0; i < users.length; i++) {
+        if (username === users[i].name) {
+            alert("Username already exists");
+            return;
+        }
+    }
+    users.push({ name: username, password: password });
+    window.location.href = "index.html";
+}
+
+function cancelRegister() {
+    window.location.href = "index.html";
+}
